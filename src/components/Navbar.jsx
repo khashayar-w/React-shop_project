19c7953b001b28c9dd1f2aa0f003f4c15d5518e5
khashayar <work82887@gmail.com>
2024-09-08 10:28:55 +0330

    project-num-2(Navbr full responsive/without count number)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,66 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+//*logos and icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import{faBagShopping ,faSearch} from "@fortawesome/free-solid-svg-icons"
 
-import {faShoppingCart,faBars} from "@fortawesome/free-solid-svg-icons"
-import { useState } from "react";
-//*state management by context
-import { profileContext } from "./context";
-import { useContext } from "react";
-
-
-const NavBar = () => {
-    const [isVisible,setIsVisible] = useState(false)
-    return(
-        <div className="navbar navbar-dark bg-dark navbar-expand-lg">
-            <div className="container">
-                <a className="navbar-brand">khashayr</a>
-                <ul className="navbar-nav">
-                    <li className="nav-item ">
-                        <Link className=" nav-link text-light" to={'/'}>Home</Link>
-                        
-                    </li>
-                    <li className="nav-item text-light">
-                        <Link className=" nav-link text-light " to={'/cart'}>cart <FontAwesomeIcon icon={faShoppingCart}/></Link>
-                        
-                    </li>      
-                </ul>
-                <button onClick={()=>setIsVisible(!isVisible)}><FontAwesomeIcon icon={faBars}/></button>
-                {isVisible &&(
-                    <div >
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link" to={'/'}>Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to={'/'}>About us</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to={'/'}>News</Link>
-                        </li>
-                    </ul>
-                    </div>
-                )}
+const Navbar = () => {
+    //*states
+    const[isVisible,setIsVisible]=useState(false)
+    //*functions
+    const visible = ()=>{
+        setIsVisible(!isVisible)
+        
+    }
+    return ( 
+        <React.Fragment>
+            <nav class="navbar navbar-expand-lg bg-danger ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Khashayar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+            <Link className=" nav-link active" aria-current="page" to="/store">Store</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/support">Support</Link>
+        </li>
+        <li class="nav-item dropdown">
+          <Link onClick={visible} class="nav-link dropdown-toggle"  role="button" >
+            Dropdown
+          </Link>
+          {!isVisible && (
+            <div>
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">About Us</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">More</a>
+                </li>
+              </ul>
             </div>
-        </div>
-    )
+          )}
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link " aria-disabled="true" to='/cart'><FontAwesomeIcon icon={faBagShopping}/></Link>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success btn-warning" type="submit"><FontAwesomeIcon icon={faSearch}/></button>
+      </form>
+    </div>
+  </div>
+</nav>
+        </React.Fragment>
+     );
 }
  
-export default NavBar;
+export default Navbar;
